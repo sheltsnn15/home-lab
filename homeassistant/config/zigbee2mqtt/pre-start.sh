9678a86b975bcb4e3fc31e1862875e1bf61f736d
Shelton Ngwenya <shelt15.nn@gmail.com>
2026-02-03 14:51:03 +0000

    Clean repo: ignore generated data, caches, and secrets

--- homeassistant/config/zigbee2mqtt/pre-start.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-set -e
-
-# Source the .env file
-source ../.env
-
-# Replace placeholders in configuration.yaml
-sed -i "s|\${MQTT_USERNAME}|${MQTT_USERNAME}|g" /app/data/configuration.yaml
-sed -i "s|\${MQTT_PASSWORD}|${MQTT_PASSWORD}|g" /app/data/configuration.yaml
